baf257e884e3e701778467c0663cf6bc6e9063f5
Ben Beattie <157050521+bbeattie-1@users.noreply.github.com>
2024-10-26 01:07:24 +0000

    commit

--- script.js
+++ script.js
@@ -1,73 +1,100 @@
-require(["esri/layers/GraphicsLayer", "esri/Graphic", "esri/geometry/Point"], (GraphicsLayer, Graphic, Point) => { 
+require(["esri/layers/GraphicsLayer", "esri/Graphic", "esri/geometry/Point"], (GraphicsLayer, Graphic, Point) => {
 
-    const map = document.getElementById("map")
-    const searchButton = document.getElementById("search-button")
-    const resetButton = document.getElementById("reset-button")
-    const searchInput = document.getElementById("search-input")
+    const map = document.getElementById("map");
+    const searchButton = document.getElementById("search-button");
+    const resetButton = document.getElementById("reset-button");
+    const searchInput = document.getElementById("search-input");
+    const locationsList = document.getElementById("locations-list");
+    const locationsListLabel = document.getElementById("location-list-label");
 
-    resetButton.addEventListener("click", function () {
-        searchInput.value = ""
-        searchButton.disabled = true
-    })
+    const apiKey = '3NKHt6i2urmWtqOuugvr9XZEWL72h9Wu8zfpFFWCjgz_A1o2-jMTn4o1myyop1OWJjGmzdueCT0FnnsJA0tsXYnWHdLcOr-5QAg12wXKH2-s0EU5M6Itx0VUlQ6R3Rns';
 
-    searchInput.addEventListener("calciteInputTextInput", function() {
-        if (searchInput.value.length > 2) {
-            searchButton.disabled = false
-        } else {
-            searchButton.disabled = true
-        }
-    })
-    
-    
-    searchButton.addEventListener("click", function () {
-        const apiKey = '3NKHt6i2urmWtqOuugvr9e47A_MlN1O7L2-Hi4TkVsAgdQFUFdjM0lLJc0Z6ft7oIqRRMiWG1MGwHnj4dYITWMZi5sHNnYU3WKLFBtRFYqwaD3GKF1-_FPwr9JdGnDGI'
-        let searchText = searchInput.value
-        let searchURL = `https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?address=${searchText}&outFields=*&f=json&token=${apiKey}`
+    resetButton.addEventListener("click", resetSearch);
+    searchInput.addEventListener("calciteInputTextInput", limitSearch);
+    searchButton.addEventListener("click", performSearch);
+
+    function resetSearch() {
+        searchInput.value = "";
+        searchButton.disabled = true;
+    }
+
+    function limitSearch() {
+        searchButton.disabled = searchInput.value.length <= 2;
+    }
+
+    async function performSearch() {
+        const searchText = searchInput.value;
+        const searchURL = buildSearchURL(searchText);
         
-        let geocodeLocation;
-
-        async function geocode() {
-            try {
-                const response = await fetch(searchURL)
-                const geocodeJSON = await response.json()
-                const geocodedAddressResult = await geocodeJSON.candidates[0]
-                const { address, extent, location} = geocodedAddressResult
-                geocodeLocation = await location
-                await map.goTo({
-                    target: [location.x, location.y],
-                    zoom: 9}, {
-                        duration: 4000
-                    })
-            } 
-            catch (error) {
-                console.error(error.message)
-            }
+        try {
+            const geocodeResult = await geocodeAddress(searchURL);
+            await moveToLocation(geocodeResult.location);
+            addGraphicToMap(geocodeResult.location);
+            updateLocationList(geocodeResult.address);
+        } catch (error) {
+            console.error(error.message);
         }
-    
-        geocode().then(function() {
-            console.log(geocodeLocation);
-            const geocodeGraphic = new Graphic({
-                geometry: new Point({
-                    latitude: geocodeLocation.y,
-                    longitude: geocodeLocation.x
-                }),
-                symbol: {
-                    type: "simple-marker",
-                    color: [0, 0, 255, 1],
-                    size: 30,
-                    style: "circle",
-                    outline: {
-                        width: 1,
-                        color: [255, 255, 255, 1]
-                    }
-                },
-            })
-    
-            const geocodeGraphicsLayer = new GraphicsLayer({
-                graphics: [geocodeGraphic]
-            })
+    }
 
-            map.addLayer(geocodeGraphicsLayer)
-        })
-    })
- });
+    function buildSearchURL(address) {
+        return `https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?address=${address}&outFields=*&f=json&token=${apiKey}`;
+    }
+
+    async function geocodeAddress(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        
+        const candidate = data.candidates[0];
+        return {
+            address: candidate.address,
+            location: candidate.location
+        };
+    }
+
+    async function moveToLocation(location) {
+        await map.goTo({
+            target: [location.x, location.y],
+            zoom: 9
+        }, {
+            duration: 2000
+        });
+    }
+
+    function addGraphicToMap(location) {
+        const graphic = new Graphic({
+            geometry: new Point({
+                latitude: location.y,
+                longitude: location.x
+            }),
+            symbol: {
+                type: "simple-marker",
+                color: [0, 0, 255, 1],
+                size: 30,
+                style: "circle",
+                outline: {
+                    width: 1,
+                    color: [255, 255, 255, 1]
+                }
+            }
+        });
+
+        const graphicsLayer = new GraphicsLayer({
+            graphics: [graphic]
+        });
+
+        map.addLayer(graphicsLayer);
+    }
+
+    function updateLocationList(address) {
+        locationsListLabel.hidden = false;
+        const listItem = document.createElement("calcite-list-item");
+        const listItemIcon = document.createElement("calcite-icon");
+        listItemIcon.setAttribute("icon", "pin-tear-f");
+        listItemIcon.setAttribute("scale", "s");
+        listItemIcon.setAttribute("slot", "content-start");
+        listItem.setAttribute("label", address);
+        listItem.appendChild(listItemIcon);
+        locationsList.appendChild(listItem);
+    }
+});
